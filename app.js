d9288f9fcb926a4d447749f9b57c4f3c8860c8f2
Yanis Mohamed <yanismxd@gmail.com>
2024-05-28 13:42:01 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import pg from 'pg';
+import 'dotenv/config';
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+export async function dbConnect () {
+
+  const client = new pg.Client({
+    user: PGUSER,
+    host: PGHOST,
+    database: PGDATABASE,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: 'require',
+    connection: {
+      options: `project=${ENDPOINT_ID}`,
+    },
+    });
+   return client;
+}
+
---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { dbConnect } from './app.js'; // Importez votre configuration PostgreSQL
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+
+dbConnect().then((client) => {
+  console.log('Connected to PostgreSQL database');
+});
