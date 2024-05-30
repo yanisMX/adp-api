11098064bbb65b62b403ec350506fa75010e08f6
Nino HERRAN <dev@ninoherran.fr>
2024-05-30 12:10:22 +0200

    chore : swagger correction

--- infra/swagger.js
+++ infra/swagger.js
@@ -1,29 +1,27 @@
-// swagger.js
-
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 
 // Définir les options de Swagger
 const swaggerOptions = {
-  swaggerDefinition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'Documentation de notre API ADP',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-        description: 'Serveur de développement',
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], // Chemin vers vos fichiers de route
+	swaggerDefinition: {
+		openapi: "3.0.0",
+		info: {
+			title: "API Documentation",
+			version: "1.0.0",
+			description: "Documentation de notre API ADP",
+		},
+		servers: [
+			{
+				url: "http://localhost:3001",
+				description: "Serveur de développement",
+			},
+		],
+	},
+	apis: ["./endpoint/controller/*.js"],
 };
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
-module.exports = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+export default (app) => {
+	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 };
